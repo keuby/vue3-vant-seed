e295db3c5e966cb75e22b62d08012e9f1c4ccbd1
lnfsink <lnfsink@sina.com>
2021-04-19 12:02:31 +0800

    fix: 修复当网络异常时 env config 值获取错误的问题C

--- src/plugins/env/fetch.ts
+++ src/plugins/env/fetch.ts
@@ -1,12 +1,11 @@
+import { SafeAny } from '@/utils';
 import { Ref, ref } from '@vue/runtime-core';
 import { isFunction } from 'lodash';
 import { EnvConfig, DEFAULT_CONFIG_URL, EnvConfigOption } from './util';
 
 const state = {
-  v: ref({
-    SERVER_URL: 'null',
-  }) as Ref<EnvConfig>,
-  p: null as Promise<EnvConfig>,
+  v: ref({}) as Ref<EnvConfig>,
+  d: null as [Promise<EnvConfig>, (c: EnvConfig) => void],
   f: false,
 };
 
@@ -31,9 +30,9 @@ export function getEnvConfig(callback?: (config: EnvConfig) => void): Promise<En
     }
   } else {
     if (isFunction(callback)) {
-      state.p.then(callback);
+      state.d[0].then(callback);
     } else {
-      return state.p;
+      return state.d[0];
     }
   }
 }
@@ -43,23 +42,25 @@ export function getEnvConfigSync(): Ref<EnvConfig> {
 }
 
 export function fetchEnvConfig(option?: EnvConfigOption): Ref<EnvConfig> {
-  if (state.f || state.p != null) return state.v;
+  if (state.f || state.d) return state.v;
 
-  const url = getEnvConfigUrl(option);
-  state.p = fetch(url).then((resp) => resp.json());
+  state.d = [] as SafeAny;
+  state.d[0] = new Promise<EnvConfig>((resolve) => (state.d[1] = resolve));
 
-  state.p.then((config) => {
-    state.f = true;
-    state.v.value = config;
-  });
+  let doFetch = () => {
+    const url = getEnvConfigUrl(option);
+    const p = fetch(url).then((resp) => resp.json());
 
-  state.p.catch(() => {
-    setTimeout(() => fetchEnvConfig(option), 2000);
-  });
+    p.then((config) => {
+      doFetch = null;
+      state.f = true;
+      state.v.value = config;
+      state.d[1](config);
+    });
 
-  state.p.finally(() => {
-    state.p = null;
-  });
+    p.catch(() => setTimeout(doFetch, 2000));
+  };
 
+  doFetch();
   return state.v;
 }
